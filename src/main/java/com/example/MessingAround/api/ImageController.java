1ed8c25088bd4cf189f78c64cd0fc6fb029f7bef
Downster <b.downing.code@gmail.com>
2022-10-04 12:46:12 -0700

    Adding Image routes

--- src/main/java/com/example/MessingAround/api/ImageController.java
+++ src/main/java/com/example/MessingAround/api/ImageController.java
@@ -1,7 +1,23 @@
 package com.example.MessingAround.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.MessingAround.model.Image;
+import com.example.MessingAround.service.ImageService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
+@RestController
+@RequestMapping("/image")
+@CrossOrigin("*")
 public class ImageController {
+
+    ImageService service;
+
+    @GetMapping
+    public ResponseEntity<List<Image>> getImages() {
+        return new ResponseEntity<>(service.getAllTodos(), HttpStatus.OK);
+    }
+
 }
